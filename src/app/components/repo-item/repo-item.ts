4f8ca3dec09532586d8cbc797c436c66548637eb
Martin Leathem <mleathem@users.noreply.github.com>
2026-02-09 17:34:27 +0100

    Fix 'Submitted days ago by'

--- src/app/components/repo-item/repo-item.ts
+++ src/app/components/repo-item/repo-item.ts
@@ -1,4 +1,4 @@
-import { Component, input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, input, ChangeDetectionStrategy, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GithubRepository } from '../../models/github-repo.interface';
 
@@ -11,4 +11,18 @@ import { GithubRepository } from '../../models/github-repo.interface';
 })
 export class RepoItemComponent {
   repository = input.required<GithubRepository>();
+
+  daysAgoText = computed(() => {
+    const pushed = new Date(this.repository().pushed_at).getTime();
+    const now = Date.now();
+    const diffMs = now - pushed;
+    const days = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+
+    // In the case of 'Submitted 0 days ago' make this more user friendly
+    if (days === 0) {
+      return 'Submitted today';
+    }
+
+    return `Submitted ${days} days ago`;
+  });
 }
